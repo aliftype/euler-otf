1980d04ae05a1410554b6b12ad8aec0329ba4323
Khaled Hosny <khaledhosny@eglug.org>
2009-11-01 16:25:56 +0200

    Merge the 3 scripts into one file

--- utils/set_ssty.py
+++
@@ -1,7 +0,0 @@
-def set_ssty(data, font):
-    selection = font.selection.byGlyphs
-    for glyph in selection:
-        name = glyph.glyphname
-        glyph.addPosSub("'ssty' Script Style lookup 0-1",(name+".ssty1",name+".ssty2"))
-
-fontforge.registerMenuItem(set_ssty,None,None,"Font",None,"Set SSTY variants")
